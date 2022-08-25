8034ebcdc8b15cb8405aa48c6021f6d2371e98c9
82108 <82108@DESKTOP-ABHK1M5>
2022-08-26 02:05:02 +0900

    0000

--- front/public/밥/0825.js
+++ front/public/밥/0825.js
@@ -1,45 +1,40 @@
- //객체명.addEventListener('이벤트명' , 콜백함수)
- 	//해당 객체의 이벤트 발생했을 때 콜백함수 실행된다~
- 	
- 
- let counter = 0	//클릭수를 저장하는 변수
- let isConnect = false	//연결 상태를 저장하는 변수
- 
- //요소 객체들
- const h1 = document.querySelector('h1')	//요소 가져오기
- const p = document.querySelector('p')		//요소 가져오기
- const connectBtn = document.querySelector('#connect')		//요소 가져오기
- const disconnectBtn = document.querySelector('#disconnect')	//요소 가져오기
- 
- //출력하는 함수
- const listener = () => {
-		h1.textContent = `클릭횟수 : ${++counter}`		//달러쓸땐 무조건 백틱!
-}
 
-//1. 연결 버튼을 클릭했을때
-connectBtn.addEventListener('click', () => {
-	if( isConnect == false ){				//만약에 연결이 안되어 있으면
-		h1.addEventListener('click' , listener )	// h1태그에 클릭 이벤트 추가
-		p.textContent = '이벤트 연결 상태 : 연결'	// p 태그에 내용 넣기
-		isConnect = true	//연결 했다는 의미로 연결상태 변수에 ture 로 변환
-	}
-})
+/*
+	왼쪽 방향키 = 37 위쪽 방향키 = 38 오른쪽 방향키 = 39 아래쪽 방향키 = 40
+*/
 
-// 2. 연결해제 버튼을 클릭했을때
-disconnectBtn.addEventListener('click' , () => {
-	if( isConnect == true ){		//만약에 연결이 되어 있으면
-	h1.removeEventListener('click' , listener)	//h1태그에 클릭 이벤트 제거
-	p.textContent = '이벤트 연결 상태 해제'
-	isConnect = false
-		
-	}
-})
+//별의 초기 설정
 
+const star = document.querySelector('h1')	//문서객체 가져오기
+star.style.position="absolute"		//css 조작 = 포지션 : 절대위치
 
-//2. html에서 클릭이벤트 속성 이용한다.
-function 함수(){
-	counter++	//변수에 1증가
-	h1.textContent = `클릭횟수 : ${counter}`
-}
+//별의 이동을 출력하는 기능
+let [ x , y ] = [ 0 , 0 ]
+const block = 10	//이동단위
+const print = () => {
+	star.style.left = `${x*block}px`
+	star.style.top = `${y*block}px`
 	
+}
 
+print()
+
+const [ left , up , right , down ] = [ 37 , 38 , 39 , 40 ]
+
+document.body.addEventListener( 'keydown' , (event) => {
+	switch( event.keyCode ){
+		case left :
+			x -= 1
+			break
+		case up :
+			y -= 1
+			break
+		case left :
+			x += 1
+			break
+		case left :
+			y += 1
+			break
+	}
+	print()
+})
