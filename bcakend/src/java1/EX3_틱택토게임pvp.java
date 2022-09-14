a6629c47520ad9bd2c160d7d5b27fd4c298084cd
JunJungyu <110512929+JunJungyu@users.noreply.github.com>
2022-09-15 00:28:04 +0900

    틱택토

--- bcakend/src/java1/EX3_틱택토게임pvp.java
+++ bcakend/src/java1/EX3_틱택토게임pvp.java
@@ -3,59 +3,55 @@ package java1;
 import java.util.Random;
 import java.util.Scanner;
 
-public class EX3_틱택토게임 {//class s
+public class EX3_틱택토게임pvp {//class s
 
 	public static void main(String[] args) {//main s
 		Scanner scanner = new Scanner(System.in);   
 	      
 	      String[] 게임판 = { "[ ]","[ ]","[ ]","[ ]","[ ]","[ ]","[ ]","[ ]","[ ]" };
 	      boolean game = true;
-	      int 승리 = 0; 
-	      while(game) {
-        	
-			   for( int j = 0; j < 7 ; j += 3 ) {//012 345 678  
+         while(game) {
+        	 
+        	 	
+        	 int 승리 = 0; 
+			   for( int j = 0; j < 7 ; j += 3 ) {//012 345 678
 				   if( 게임판[j].equals(게임판[j+1]) && 게임판[j+1].equals(게임판[j+2]) && !게임판[j].equals("[ ]") && !게임판[j+1].equals("[ ]") && !게임판[j+2].equals("[ ]") ) {
-					  if( 승리 % 2 == 1 ) {
-					   System.out.println("1흑돌 승리입니다.");
+					  if( 승리 == 0 ) {
+					   System.out.println("흑돌 승리입니다.");
 					   return; 
-					  }else if( 승리 % 2 == 0 ){System.out.println("2백돌 승리입니다.");return;}
+					  }else if( 승리 == 1 ){System.out.println("백돌 승리입니다.");}
 					  
 				   }
 			  }
 				   
 			   for( int y = 0; y < 2 ; y++ ) {//036 147 258
 				   if( 게임판[y].equals(게임판[y+3]) && 게임판[y+3].equals(게임판[y+6]) && !게임판[y].equals("[ ]") && !게임판[y+3].equals("[ ]") && !게임판[y+6].equals("[ ]") ) {
-						  if( 승리 % 2 == 1 ) {
-							   System.out.println("3흑돌 승리입니다.");
+						  if( 승리 == 0 ) {
+							   System.out.println("흑돌 승리입니다.");
 							   return; 
-							  }else if( 승리 % 2 == 0 ){System.out.println("4백돌 승리입니다.");}
+							  }else if( 승리 == 1 ){System.out.println("백돌 승리입니다.");}
 					   return;
 				   }
 			   }
 
 			   if( 게임판[0].equals(게임판[4]) && 게임판[4].equals(게임판[8]) && !게임판[0].equals("[ ]") && !게임판[4].equals("[ ]") && !게임판[8].equals("[ ]") ) {//048
-					  if( 승리 % 2 == 1 ) {
-						   System.out.println("5흑돌 승리입니다.");
+					  if( 승리 == 0 ) {
+						   System.out.println("흑돌 승리입니다.");
 						   return; 
-						  }else if( 승리 % 2 == 0 ){System.out.println("6백돌 승리입니다.");}
+						  }else if( 승리 == 1 ){System.out.println("백돌 승리입니다.");}
 				   return;
 			   }
 			
 			   if( 게임판[2].equals(게임판[4]) && 게임판[4].equals(게임판[6]) && !게임판[2].equals("[ ]") && !게임판[4].equals("[ ]") && !게임판[6].equals("[ ]") ) {//048 246
-					  if( 승리 % 2 == 1 ) {
-						   System.out.println("7흑돌 승리입니다.");
+					  if( 승리 == 0 ) {
+						   System.out.println("흑돌 승리입니다.");
 						   return; 
-						  }else if( 승리 % 2 == 0 ){System.out.println("8백돌 승리입니다.");}
+						  }else if( 승리 == 1 ){System.out.println("백돌 승리입니다.");}
 				   return;
-			   }		
+			   }			   
 			   
-			   	   
-			   		   if( 승리 == 9 ) {
-			   			   System.out.println("무승부입니다.");
-			   		   }
-			   	   
 				   
-			   
+				   
                      // 1. 게임판 출력 - 배열내 모든 데이터 출력 
                for( int i=0; i<9; i++ ) {
                       System.out.print(게임판[i]);
@@ -64,43 +60,29 @@ public class EX3_틱택토게임 {//class s
                       }
 	           }
 	           while (true) {
-	        	   
-				   System.out.println("흑돌 위치를 선택해주세요."+승리); 
-				   int 흑돌 = scanner.nextInt();
-				   if( 흑돌<8 || 흑돌>0 ) {
-					   if( 게임판[흑돌].equals("[ ]") && !게임판[흑돌].equals("[○]") && !게임판[흑돌].equals("[●]") ) 
-					   게임판[흑돌] = "[●]";
-					   승리++;
-					   System.out.println("혹시 여기니");
-					   break;
-					   }
-				   if( 게임판[흑돌].equals("[○]") || 게임판[흑돌].equals("[●]") ) {
-					   System.out.println("이미 돌이 존재하는 자리입니다.");
-				   
-				   }else {System.out.println("0~8 사이의 값을 입력해주세요."); break;}
-	           	}//while e
-				   
-				   while (true) {
-					
-					Random 랜덤 = new Random(); 
-				    int 백 = 랜덤.nextInt(9); 
-				    if( 게임판[백].equals("[ ]") && !게임판[백].equals("[●]") && !게임판[백].equals("[○]") ) {
-					   
-					   게임판[백] = "[○]";
-					   승리++;
-					   System.out.println("백돌차례끝"+승리);
-					   break;  
+				   System.out.println("흑돌 위치를 선택해주세요."); 
+				   int 흑돌 = scanner.nextInt();   
+				   if( 게임판[흑돌].equals("[ ]") && !게임판[흑돌].equals("[○]") && !게임판[흑돌].equals("[●]") ) {
+				   게임판[흑돌] = "[●]";
 				   }
-				     
-				   }
+				   ++승리;
 				   
+			
 				
 				   
-				 
+				   System.out.println("백돌 위치를 선택해주세요.");
+				   int 백돌 = scanner.nextInt();
+				   if( 게임판[백돌].equals("[ ]") && !게임판[백돌].equals("[●]") && !게임판[백돌].equals("[○]") ) {
+					   게임판[백돌] = "[○]";
+					   --승리;
+					   break;  
+				   }else{System.out.println("이미 돌이 존재하는 자리입니다.");
+					   break;
+				   }
 				
 
 	           
-			     
+			  }//while e   
 	     }//while e
 	}//main e
 }//calss e
